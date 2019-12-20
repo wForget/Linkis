c9101a2d84886be63c3a3ca93fbeb2a6a0f89e12
wang_zh <643348094@qq.com>
2019-12-20 12:00:55 +0800

    打包冲突

--- ujes/definedEngines/mlsql/entrance/bin/start-mlsql-entrance.sh
+++ ujes/definedEngines/mlsql/entrance/bin/start-mlsql-entrance.sh
@@ -17,7 +17,7 @@ fi
 
 export DWS_ENGINE_MANAGER_LOG_PATH=$HOME/logs
 export DWS_ENGINE_MANAGER_HEAP_SIZE="1G"
-export DWS_ENGINE_MANAGER_JAVA_OPTS="-Xms$DWS_ENGINE_MANAGER_HEAP_SIZE -Xmx$DWS_ENGINE_MANAGER_HEAP_SIZE -XX:+UseG1GC -XX:MaxPermSize=500m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=11716"
+export DWS_ENGINE_MANAGER_JAVA_OPTS="-Xms$DWS_ENGINE_MANAGER_HEAP_SIZE -Xmx$DWS_ENGINE_MANAGER_HEAP_SIZE -XX:+UseG1GC -XX:MaxPermSize=500m"
 
 nohup java $DWS_ENGINE_MANAGER_JAVA_OPTS -cp $HOME/conf:$HOME/lib/* com.webank.wedatasphere.linkis.DataWorkCloudApplication 2>&1 > $DWS_ENGINE_MANAGER_LOG_PATH/linkis.out &
 pid=$!
