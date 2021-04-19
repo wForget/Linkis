b83da87fd845f2e3c2dab985b68dc363cb0636fd
Wang Zhen <wangzhen07@qiyi.com>
2021-04-19 11:46:42 +0800

    commit

--- linkis-engineconn-plugins/engineconn-plugins/presto/src/main/scala/com/webank/wedatasphere/linkis/engineplugin/presto/executer/PrestoEngineConnExecutor.scala
+++ linkis-engineconn-plugins/engineconn-plugins/presto/src/main/scala/com/webank/wedatasphere/linkis/engineplugin/presto/executer/PrestoEngineConnExecutor.scala
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit
 import com.facebook.presto.client.{ClientSession, QueryStatusInfo, StatementClient, StatementClientFactory}
 import com.facebook.presto.spi.security.SelectedRole
 import com.google.common.cache.{Cache, CacheBuilder}
-import com.webank.wedatasphere.linkis.common.io.FsPath
 import com.webank.wedatasphere.linkis.common.log.LogUtils
 import com.webank.wedatasphere.linkis.common.utils.Utils
 import com.webank.wedatasphere.linkis.engineconn.common.conf.{EngineConnConf, EngineConnConstant}
@@ -17,18 +16,18 @@ import com.webank.wedatasphere.linkis.engineconn.computation.executor.entity.Eng
 import com.webank.wedatasphere.linkis.engineconn.computation.executor.execute.{ConcurrentComputationExecutor, EngineExecutionContext}
 import com.webank.wedatasphere.linkis.engineconn.computation.executor.parser.SQLCodeParser
 import com.webank.wedatasphere.linkis.engineplugin.presto.conf.PrestoConfiguration._
+import com.webank.wedatasphere.linkis.engineplugin.presto.exception.{PrestoClientException, PrestoStateInvalidException}
 import com.webank.wedatasphere.linkis.manager.common.entity.resource.NodeResource
 import com.webank.wedatasphere.linkis.manager.label.entity.Label
-import com.webank.wedatasphere.linkis.manager.label.entity.engine.{EngineRunTypeLabel, UserCreatorLabel}
+import com.webank.wedatasphere.linkis.manager.label.entity.engine.UserCreatorLabel
 import com.webank.wedatasphere.linkis.protocol.engine.JobProgressInfo
 import com.webank.wedatasphere.linkis.rpc.Sender
 import com.webank.wedatasphere.linkis.scheduler.executer.{AliasOutputExecuteResponse, ExecuteResponse, SuccessExecuteResponse}
 import com.webank.wedatasphere.linkis.storage.domain.Column
-import com.webank.wedatasphere.linkis.storage.resultset.{ResultSetFactory, ResultSetWriter}
+import com.webank.wedatasphere.linkis.storage.resultset.ResultSetFactory
 import com.webank.wedatasphere.linkis.storage.resultset.table.{TableMetaData, TableRecord}
 import okhttp3.OkHttpClient
 import org.apache.commons.io.IOUtils
-import org.apache.commons.lang3.StringUtils
 import org.springframework.util.CollectionUtils
 
 import scala.collection.JavaConverters._
@@ -58,24 +57,23 @@ class PrestoEngineConnExecutor(override val outputPrintLimit: Int, val id: Int) 
     val realCode = code.trim
     info(s"presto client begins to run psql code:\n $realCode")
 
-    val clientSession = clientSessionCache.getIfPresent(engineExecutorContext.getJobId)
+    val taskId = engineExecutorContext.getJobId.get
+
+    val clientSession = clientSessionCache.getIfPresent(taskId)
     val statement = StatementClientFactory.newStatementClient(okHttpClient, clientSession, realCode)
 
-//    initialStatusUpdates(statement)
-//
-//    var response: ExecuteResponse = SuccessExecuteResponse()
-//    if (statement.isRunning || (statement.isFinished && statement.finalStatusInfo().getError == null)) {
-//      response = queryOutput(statement, storePath, alias)
-//    }
-//
-//    verifyServerError(statement)
-//
-//    updateSession(statement)
+    initialStatusUpdates(engineExecutorContext, statement)
 
-//    response
+    if (statement.isRunning || (statement.isFinished && statement.finalStatusInfo().getError == null)) {
+      queryOutput(engineExecutorContext, statement)
+    }
 
-    // TODO
-    ???
+    verifyServerError(engineExecutorContext, statement)
+
+    // update session
+    clientSessionCache.put(taskId, updateSession(clientSession, statement))
+
+    SuccessExecuteResponse()
   }
 
   override def executeCompletely(engineExecutorContext: EngineExecutionContext, code: String, completedLine: String): ExecuteResponse = null
@@ -149,117 +147,115 @@ class PrestoEngineConnExecutor(override val outputPrintLimit: Int, val id: Int) 
       .asInstanceOf[UserCreatorLabel]
   }
 
-//  def initialStatusUpdates(statement: StatementClient): Unit = {
-//    while (statement.isRunning
-//      && (statement.currentData().getData == null || statement.currentStatusInfo().getUpdateType != null)) {
-//      job.getProgressListener.foreach(_.onProgressUpdate(job, progress(), getProgressInfo))
-//      statement.advance()
-//    }
-//  }
-//
-//  private def queryOutput(statement: StatementClient, storePath: String, alias: String): AliasOutputExecuteResponse = {
-//    var columnCount = 0
-//    var rows = 0
-//    val resultSet = ResultSetFactory.getInstance.getResultSetByType(ResultSetFactory.TABLE_TYPE)
-//    val resultSetPath = resultSet.getResultSetPath(new FsPath(storePath), alias)
-//    val resultSetWriter = ResultSetWriter.getResultSetWriter(resultSet, PrestoConfiguration.ENTRANCE_RESULTS_MAX_CACHE.getValue.toLong, resultSetPath, job.getUser)
-//    Utils.tryFinally({
-//      var results: QueryStatusInfo = null
-//      if (statement.isRunning) {
-//        results = statement.currentStatusInfo()
-//      } else {
-//        results = statement.finalStatusInfo()
-//      }
-//      if (results.getColumns == null) {
-//        throw new RuntimeException("presto columns is null.")
-//      }
-//      val columns = results.getColumns.asScala
-//        .map(column => Column(column.getName, column.getType, "")).toArray[Column]
-//      columnCount = columns.length
-//      resultSetWriter.addMetaData(new TableMetaData(columns))
-//      while (statement.isRunning) {
-//        val data = statement.currentData().getData
-//        if (data != null) for (row <- data.asScala) {
-//          val rowArray = row.asScala.map(r => String.valueOf(r))
-//          resultSetWriter.addRecord(new TableRecord(rowArray.toArray))
-//          rows += 1
-//        }
-//        job.getProgressListener.foreach(_.onProgressUpdate(job, progress(), getProgressInfo))
-//        statement.advance()
-//      }
-//    })(IOUtils.closeQuietly(resultSetWriter))
-//
-//    info(s"Fetched $columnCount col(s) : $rows row(s) in presto")
-//    job.getLogListener.foreach(_.onLogUpdate(job, LogUtils.generateInfo(s"Fetched $columnCount col(s) : $rows row(s) in presto")))
-//    val output = if (resultSetWriter != null) resultSetWriter.toString else null
-//    AliasOutputExecuteResponse(alias, output)
-//  }
-//
-//  // check presto error
-//  private def verifyServerError(statement: StatementClient): Unit = {
-//    job.getProgressListener.foreach(_.onProgressUpdate(job, progress(), getProgressInfo))
-//    if (statement.isFinished) {
-//      val info: QueryStatusInfo = statement.finalStatusInfo()
-//      if (info.getError != null) {
-//        val error = Objects.requireNonNull(info.getError);
-//        val message: String = s"Presto execute failed (#${info.getId}): ${error.getMessage}"
-//        var cause: Throwable = null
-//        if (error.getFailureInfo != null) {
-//          cause = error.getFailureInfo.toException
-//        }
-//        throw new SQLException(message, error.getSqlState, error.getErrorCode, cause)
-//      }
-//    } else if (statement.isClientAborted) {
-//      warn(s"Presto statement is killed by ${job.getUser}")
-//    } else if (statement.isClientError) {
-//      throw PrestoClientException("Presto client error.")
-//    } else {
-//      throw PrestoStateInvalidException("Presto status error. Statement is not finished.")
-//    }
-//  }
-//
-//  private def updateSession(statement: StatementClient): Unit = {
-//    var newSession = clientSession.get()
-//    // update catalog and schema if present
-//    if (statement.getSetCatalog.isPresent || statement.getSetSchema.isPresent) {
-//      newSession = ClientSession.builder(newSession)
-//        .withCatalog(statement.getSetCatalog.orElse(newSession.getCatalog))
-//        .withSchema(statement.getSetSchema.orElse(newSession.getSchema))
-//        .build
-//    }
-//
-//    // update transaction ID if necessary
-//    if (statement.isClearTransactionId) newSession = ClientSession.stripTransactionId(newSession)
-//
-//    var builder: ClientSession.Builder = ClientSession.builder(newSession)
-//
-//    if (statement.getStartedTransactionId != null) builder = builder.withTransactionId(statement.getStartedTransactionId)
-//
-//    // update session properties if present
-//    if (!statement.getSetSessionProperties.isEmpty || !statement.getResetSessionProperties.isEmpty) {
-//      val sessionProperties: util.Map[String, String] = new util.HashMap[String, String](newSession.getProperties)
-//      sessionProperties.putAll(statement.getSetSessionProperties)
-//      sessionProperties.keySet.removeAll(statement.getResetSessionProperties)
-//      builder = builder.withProperties(sessionProperties)
-//    }
-//
-//    // update session roles
-//    if (!statement.getSetRoles.isEmpty) {
-//      val roles: util.Map[String, SelectedRole] = new util.HashMap[String, SelectedRole](newSession.getRoles)
-//      roles.putAll(statement.getSetRoles)
-//      builder = builder.withRoles(roles)
-//    }
-//
-//    // update prepared statements if present
-//    if (!statement.getAddedPreparedStatements.isEmpty || !statement.getDeallocatedPreparedStatements.isEmpty) {
-//      val preparedStatements: util.Map[String, String] = new util.HashMap[String, String](newSession.getPreparedStatements)
-//      preparedStatements.putAll(statement.getAddedPreparedStatements)
-//      preparedStatements.keySet.removeAll(statement.getDeallocatedPreparedStatements)
-//      builder = builder.withPreparedStatements(preparedStatements)
-//    }
-//
-//    clientSession.set(newSession)
-//  }
+  private def initialStatusUpdates(engineExecutorContext: EngineExecutionContext, statement: StatementClient): Unit = {
+    while (statement.isRunning
+      && (statement.currentData().getData == null || statement.currentStatusInfo().getUpdateType != null)) {
+      engineExecutorContext.pushProgress(progress(), getProgressInfo)
+      statement.advance()
+    }
+  }
+
+  private def queryOutput(engineExecutorContext: EngineExecutionContext, statement: StatementClient): Unit = {
+    var columnCount = 0
+    var rows = 0
+    val resultSetWriter = engineExecutorContext.createResultSetWriter(ResultSetFactory.TABLE_TYPE)
+    Utils.tryFinally({
+      var results: QueryStatusInfo = null
+      if (statement.isRunning) {
+        results = statement.currentStatusInfo()
+      } else {
+        results = statement.finalStatusInfo()
+      }
+      if (results.getColumns == null) {
+        throw new RuntimeException("presto columns is null.")
+      }
+      val columns = results.getColumns.asScala
+        .map(column => Column(column.getName, column.getType, "")).toArray[Column]
+      columnCount = columns.length
+      resultSetWriter.addMetaData(new TableMetaData(columns))
+      while (statement.isRunning) {
+        val data = statement.currentData().getData
+        if (data != null) for (row <- data.asScala) {
+          val rowArray = row.asScala.map(r => String.valueOf(r))
+          resultSetWriter.addRecord(new TableRecord(rowArray.toArray))
+          rows += 1
+        }
+        engineExecutorContext.pushProgress(progress(), getProgressInfo)
+        statement.advance()
+      }
+    })(IOUtils.closeQuietly(resultSetWriter))
+
+    info(s"Fetched $columnCount col(s) : $rows row(s) in presto")
+    engineExecutorContext.appendStdout(LogUtils.generateInfo(s"Fetched $columnCount col(s) : $rows row(s) in presto"));
+    engineExecutorContext.sendResultSet(resultSetWriter)
+    IOUtils.closeQuietly(resultSetWriter)
+  }
+
+  // check presto error
+  private def verifyServerError(engineExecutorContext: EngineExecutionContext, statement: StatementClient): Unit = {
+    engineExecutorContext.pushProgress(progress(), getProgressInfo)
+    if (statement.isFinished) {
+      val info: QueryStatusInfo = statement.finalStatusInfo()
+      if (info.getError != null) {
+        val error = Objects.requireNonNull(info.getError);
+        val message: String = s"Presto execute failed (#${info.getId}): ${error.getMessage}"
+        var cause: Throwable = null
+        if (error.getFailureInfo != null) {
+          cause = error.getFailureInfo.toException
+        }
+        throw new SQLException(message, error.getSqlState, error.getErrorCode, cause)
+      }
+    } else if (statement.isClientAborted) {
+      warn(s"Presto statement is killed.")
+    } else if (statement.isClientError) {
+      throw PrestoClientException("Presto client error.")
+    } else {
+      throw PrestoStateInvalidException("Presto status error. Statement is not finished.")
+    }
+  }
+
+  private def updateSession(clientSession: ClientSession, statement: StatementClient): ClientSession = {
+    var newSession = clientSession
+    // update catalog and schema if present
+    if (statement.getSetCatalog.isPresent || statement.getSetSchema.isPresent) {
+      newSession = ClientSession.builder(newSession)
+        .withCatalog(statement.getSetCatalog.orElse(newSession.getCatalog))
+        .withSchema(statement.getSetSchema.orElse(newSession.getSchema))
+        .build
+    }
+
+    // update transaction ID if necessary
+    if (statement.isClearTransactionId) newSession = ClientSession.stripTransactionId(newSession)
+
+    var builder: ClientSession.Builder = ClientSession.builder(newSession)
+
+    if (statement.getStartedTransactionId != null) builder = builder.withTransactionId(statement.getStartedTransactionId)
+
+    // update session properties if present
+    if (!statement.getSetSessionProperties.isEmpty || !statement.getResetSessionProperties.isEmpty) {
+      val sessionProperties: util.Map[String, String] = new util.HashMap[String, String](newSession.getProperties)
+      sessionProperties.putAll(statement.getSetSessionProperties)
+      sessionProperties.keySet.removeAll(statement.getResetSessionProperties)
+      builder = builder.withProperties(sessionProperties)
+    }
+
+    // update session roles
+    if (!statement.getSetRoles.isEmpty) {
+      val roles: util.Map[String, SelectedRole] = new util.HashMap[String, SelectedRole](newSession.getRoles)
+      roles.putAll(statement.getSetRoles)
+      builder = builder.withRoles(roles)
+    }
+
+    // update prepared statements if present
+    if (!statement.getAddedPreparedStatements.isEmpty || !statement.getDeallocatedPreparedStatements.isEmpty) {
+      val preparedStatements: util.Map[String, String] = new util.HashMap[String, String](newSession.getPreparedStatements)
+      preparedStatements.putAll(statement.getAddedPreparedStatements)
+      preparedStatements.keySet.removeAll(statement.getDeallocatedPreparedStatements)
+      builder = builder.withPreparedStatements(preparedStatements)
+    }
+
+    newSession
+  }
 
 
 }
